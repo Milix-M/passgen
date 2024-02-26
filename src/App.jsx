f64d7663d14c8d6b01c12c694a3b06ff92db2bbd
Milix-M <tujikou12dh@gmail.com>
2024-02-26 23:40:39 +0900

    add muiとか

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,59 @@
 import { useState } from 'react'
 import './App.css'
 import passwordGenerator from './generator/passwdGen'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
 
 function App () {
   const [password, setPassword] = useState('')
-  const [length, setLength]  = useState(0)
+  const [length, setLength] = useState(0)
 
+  const copyToClipboard = async () => {
+    await global.navigator.clipboard.writeText(password)
+  }
 
   return (
-    <div className='App'>
-      <h1>Passgen</h1>
-      <input type="number" min="4" onChange={(event)=>setLength(event.target.value)}/>
-      <button onClick={() => setPassword(passwordGenerator(length, "charonly"))}>
-        英数字のみで生成
-      </button>
-      <button onClick={() => setPassword(passwordGenerator(length, "symbolsInclude"))}>
-        英数字+記号で生成
-      </button>
-      <p>{password}</p>
+    <div className='App p-2 max-w-md w-full mx-auto'>
+      <h1 className='text-3xl texe-center font-bold pb-4'>Passgen</h1>
+
+      <div className='flex flex-col'>
+        <div className='m-1'>
+          <TextField
+            id='standard-basic'
+            label='桁数を入力'
+            variant='filled'
+            type='number'
+            onChange={event => setLength(event.target.value)}
+          />
+        </div>
+        <div className='m-1'>
+          <Button
+            variant='contained'
+            onClick={() => setPassword(passwordGenerator(length, 'charonly'))}
+          >
+            英数字のみで生成
+          </Button>
+        </div>
+        <div className='m-1'>
+          <Button
+            variant='contained'
+            onClick={() =>
+              setPassword(passwordGenerator(length, 'symbolsInclude'))
+            }
+          >
+            英数字+記号で生成
+          </Button>
+        </div>
+      </div>
+
+      {password && (
+        <div>
+          <p className='text-xl'>{password}</p>
+          <Button variant='contained' onClick={() => copyToClipboard()}>
+            COPY
+          </Button>
+        </div>
+      )}
     </div>
   )
 }
